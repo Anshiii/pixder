5dd7344c0c5346af749404d45276bf88ff2bbbe0
Anshi <bisexua1@foxmail.com>
2017-07-29 23:37:46 +0800

    清除cookie...

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 /**
  * Created by Anshi on 2017/7/17.
  */
-let cookie = "p_ab_id=2; p_ab_id_2=3; login_ever=yes; googtrans=/ja/en; googtrans=/ja/en; ki_t=1501121292477%3B1501121292477%3B1501121292477%3B1%3B1; ki_r=https%3A%2F%2Fwww.google.co.jp%2F; login_bc=1; PHPSESSID=26457473_bd5be1ad921a35f511d0051befde60ea; device_token=79bbbdae4099c5d5469db78a80aeabc0; is_sensei_service_user=1; user_language=ja; _ga=GA1.2.852950813.1499050195; _gid=GA1.2.76519267.1501121309; module_orders_mypage=%5B%7B%22name%22%3A%22recommended_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22everyone_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22following_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22mypixiv_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22fanbox%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22featured_tags%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22contests%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22sensei_courses%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22spotlight%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22booth_follow_items%22%2C%22visible%22%3Atrue%7D%5D; __utma=235335808.852950813.1499050195.1501121288.1501127470.19; __utmc=235335808; __utmz=235335808.1501127470.19.5.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmv=235335808.|2=login%20ever=yes=1^3=plan=normal=1^5=gender=female=1^6=user_id=26457473=1^9=p_ab_id=2=1^10=p_ab_id_2=3=1^11=lang=ja=1"
+let cookie = "input your cookie";
 let pageMap = {
   home: 'https://www.pixiv.net/',
   ////实际是收藏页.
